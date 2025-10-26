a59938f2a0d6d352a9bf91707e5926e2eaec8220
Maria Eduarda C <dudatmcosta@icloud.com>
2025-10-26 18:21:09 -0300

    Implement calculation of mu from NC data
    Calculate mu using linear regression on NC data.

--- exercicio3.m
+++ exercicio3.m
@@ -5,11 +5,17 @@ function [mu] = exercicio3(t, NC)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% digite seu codigo aqui
+t_prim_2h = t(1:3);
+NC_prim_2h = NC(1:3);
 
+y = log(NC_prim_2h);
+x = t_prim_2h;
 
-% mantenha essas duas linhas finais, subtituindo mu pelo valor que você calculou
-mu = 0;
+p = polyfit(x,y,1);
+
+calculo_mu = p(1);
+
+mu = calculo_mu;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 
